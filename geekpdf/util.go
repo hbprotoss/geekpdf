476ef689c0c6944812f1a3be9f122db34bf700c9
hbprotoss <gamespy1991@gmail.com>
2019-03-09 14:51:27 +0800

    add log

---
+++ geekpdf/util.go
@@ -0,0 +1,90 @@
+package geekpdf
+
+import (
+	"bytes"
+	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
+	"github.com/pkg/errors"
+)
+
+func SaveArticleAsPdf(article string, path string) (err error) {
+	if article == "" {
+		return errors.New("article is empty")
+	}
+	generator, err := wkhtmltopdf.NewPDFGenerator()
+	if err != nil {
+		return errors.Wrap(err, "save pdf failed")
+	}
+	generator.Dpi.Set(300)
+	generator.Orientation.Set(wkhtmltopdf.OrientationPortrait)
+
+	html := head + article + tail
+
+	page := wkhtmltopdf.NewPageReader(bytes.NewReader([]byte(html)))
+	generator.AddPage(page)
+	err = generator.Create()
+	if err != nil {
+		return errors.Wrap(err, "save pdf failed")
+	}
+
+	err = generator.WriteFile(path)
+	if err != nil {
+		return errors.Wrap(err, "save pdf failed")
+	}
+	return
+}
+
+const head = `
+<html>
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
+ <head>
+     <style type="text/css">
+         ._1kh1ihh6_0 {
+    font-weight: 400;
+    color: #4c4c4c;
+    line-height: 1.76;
+    white-space: normal;
+    word-break: normal;
+    font-size: 16px;
+    -webkit-font-smoothing: antialiased;
+    -webkit-transition: background-color .3s ease;
+    transition: background-color .3s ease;
+}
+._1kh1ihh6_0 pre {
+    margin-top: 16px;
+    padding: 35px 0 30px;
+    margin-bottom: 30px;
+    border-radius: 6px;
+    background: rgba(246,247,251,.749);
+    border: 0;
+}
+._1kh1ihh6_0 pre code {
+    font-size: 12px;
+    font-family: Consolas,Liberation Mono,Menlo,monospace,Courier;
+    display: block;
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    margin-left: 16px;
+    margin-right: 16px;
+    overflow: hidden;
+    position: relative;
+}
+._1kh1ihh6_0 img {
+    display: block;
+    max-width: 100%;
+    position: relative;
+    left: 50%;
+    -webkit-transform: translateX(-50%);
+    transform: translateX(-50%);
+    background-color: #eee;
+    vertical-align: top;
+    border-radius: 6px;
+}
+     </style>
+ </head>
+ <body>
+    <div class="_1kh1ihh6_0">
+`
+const tail = `</div>
+ </body>
+</html>
+`
--- main.go
+++ main.go
@@ -1,18 +1,83 @@
 package main
 
 import (
-	"fmt"
 	"geekpdf/geekpdf"
+	log "github.com/sirupsen/logrus"
+	"os"
 )
 
 func main() {
-	g := geekpdf.NewGeekTime("xxx", "xxx")
-	resp, err := g.Login()
+	err := Init()
 	if err != nil {
-		fmt.Printf("%v", err)
+		log.WithError(err).Error("init failed")
+	}
+
+	cellphone := "xx"
+	password := "xx"
+	path := "/Users/hbprotoss/Downloads/geek/"
+	cid := 139
+
+	g := geekpdf.NewGeekTime(cellphone, password)
+
+	loginResp, err := g.Login()
+	if err != nil {
+		log.WithError(err).Error("Login failed")
 		return
 	}
-	cookies := g.Cookies()
-	fmt.Printf("%v\n", cookies)
-	fmt.Printf("%v\n", resp)
+	log.WithFields(log.Fields{
+		"uid":     loginResp.UID,
+		"cookies": g.Cookies(),
+	}).Info("Login success")
+
+	articleList, err := g.ArticleList(cid)
+	if err != nil {
+		log.WithError(err).Error("Loading article list failed")
+		return
+	}
+	log.WithFields(log.Fields{
+		"productId": cid,
+		"count":     len(articleList),
+	}).Info("Loading article list success")
+
+	for _, article := range articleList {
+		article, err := g.Article(article.ID)
+		if err != nil {
+			log.WithError(err).WithFields(log.Fields{
+				"articleId": article.ID,
+				"title":     article.ArticleTitle,
+			}).Error("Loading article failed")
+			continue
+		}
+		log.WithFields(log.Fields{
+			"articleId": article.ID,
+			"title":     article.ArticleTitle,
+		}).Info("Loading article success")
+
+		pdfPath := path + article.ArticleTitle + ".pdf"
+		err = geekpdf.SaveArticleAsPdf(article.ArticleContent, pdfPath)
+		if err != nil {
+			log.WithError(err).WithFields(log.Fields{
+				"title":    article.ArticleTitle,
+				"filePath": pdfPath,
+			}).Error("Save pdf failed")
+			continue
+		}
+		log.WithFields(log.Fields{
+			"articleId": article.ID,
+			"title":     article.ArticleTitle,
+			"filePath":  pdfPath,
+		}).Info("Save pdf success")
+	}
+}
+
+func Init() (err error) {
+	initLog()
+	return nil
+}
+
+func initLog() {
+	//log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+	log.SetReportCaller(true)
 }
